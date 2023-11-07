2a80dadf0fc07d40629c880a1f33f91885cb5993
Dudu <115907714+Dudu-Passoni@users.noreply.github.com>
2023-11-07 09:07:45 -0300

    Add files via upload

---
+++ POOTeste/src/com/example/POOTeste.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class POOTeste {
+
+        public static void main(String[] args) {
+
+            //criação de classes
+            Caneta c1 = new Caneta("BIC", "vermelho");
+            c1.setCor("vermelho");
+            c1.setModelo("BIC Crystal");
+            c1.ponta = 0.5f;
+            c1.destampar();
+            c1.status();
+            c1.rabiscar();
+
+            Caneta c2 = new Caneta("BIC", "azul");
+            c2.ponta = 0.5f;
+            c2.tampar();
+            c2.status();
+            c2.rabiscar();
+        }
+    }
