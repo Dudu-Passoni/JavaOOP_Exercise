1f4698cdeba641b82804c763984c605d107ebd09
Dudu <115907714+Dudu-Passoni@users.noreply.github.com>
2023-11-10 08:57:12 -0300

    Delete Ultimate Fightning Championship directory

--- Ultimate Fightning Championship/src/ultimatefightingchampionship/Main.java
+++
@@ -1,8 +0,0 @@
-package ultimatefightingchampionship;
-
-public class Main {
-    public static void main(String[] args) {
-        Lutador l[] = new Lutador[6];
-        l[0] = new Lutador("Jose Aldo", "Brasil", 31, 1.70f, 61.0f, 29, 7, 1);
-    }
-}
